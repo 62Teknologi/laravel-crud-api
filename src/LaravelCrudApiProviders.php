7de7c8b6051ebf07e8d12c9274898cb134e97873
hifebriansyah <hifebriansyah@gmail.com>
2020-09-22 15:48:27 +0700

    routes update

--- src/LaravelCrudApiProviders.php
+++ src/LaravelCrudApiProviders.php
@@ -16,7 +16,7 @@ class LaravelCrudApiProviders extends ServiceProvider
         $this->app->router->group([
             'namespace' => 'EnamDuaTeknologi\LaravelCrudApi\Controllers',
         ], function ($router) {
-            require 'routes/api.php';
+            require DIR.'/routes/api.php';
         });
     }
 }
