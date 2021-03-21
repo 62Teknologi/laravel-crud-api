ea04a0d3c6e3d06ea8f52feb589922f071371526
hifebriansyah <hifebriansyah@gmail.com>
2021-03-21 13:29:37 +0700

    set requestable

---
+++ src/Traits/Requestable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace EnamDuaTeknologi\LaravelCrudApi\Traits;
+
+use Illuminate\Http\Request;
+
+trait RequestableTrait
+{
+    public $requestable = true;
+
+    public function get($path, $auth = null)
+    {
+        return $this->process(Request::create($path, 'GET'), $auth);
+    }
+
+    public function post($path, $data, $auth = null)
+    {
+        return $this->process(Request::create($path, 'POST', $data), $auth);
+    }
+
+    public function put($path, $data, $auth = null)
+    {
+        return $this->process(Request::create($path, 'PUT', $data), $auth);
+    }
+
+    public function del($path, $data, $auth = null)
+    {
+        return $this->process(Request::create($path, 'DELETE'), $auth);
+    }
+
+    private function process($request, $auth)
+    {
+        if ($auth) {
+            $request->headers->set('Authorization', 'Bearer '.$auth);
+        }
+
+        $request = app()->handle($request);
+        $response = json_decode($request->getContent(), true);
+        $response["status_code"] = $request->getStatusCode();
+        
+        if ($response["status_code"] === 401 && $auth != 'login') {
+            session()->flush();
+            return redirect(env('APP_URL').'/admin/auth/logout')->send();
+        }
+
+        return $response;
+    }
+}
